31249289c64f116bb55809c35dfcc438be400eef
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-04-15 08:16:35 +0200

    convert box to tailwind

--- src/app/collectible/[chainParam]/[collectionId]/[tokenId]/_components/ordersTable/_components/TableRow.tsx
+++ src/app/collectible/[chainParam]/[collectionId]/[tokenId]/_components/ordersTable/_components/TableRow.tsx
@@ -2,7 +2,7 @@ import { cn, Table } from '~/components/ui';
 
 import AddressPill from './AddressPill';
 import MarketplacePill from './MarketplacePill';
-import { Box, Text } from '@0xsequence/design-system';
+import { Text } from '@0xsequence/design-system';
 import { compareAddress, type Order } from '@0xsequence/marketplace-sdk';
 import { formatDistance } from 'date-fns';
 import { formatUnits, type Hex } from 'viem';
@@ -35,8 +35,8 @@ const OrdersTableRow = ({
         )}
       >
         <Table.Cell className="p-2">
-          <Box display="flex" alignItems="center" gap="10">
-            <Box display="flex" flexDirection="column" gap="1">
+          <div className="flex items-center gap-10">
+            <div className="flex flex-col gap-1">
               <Text color="text50" fontSize="xsmall" fontWeight="bold">
                 Quantity
               </Text>
@@ -44,9 +44,9 @@ const OrdersTableRow = ({
               <Text color="text100" fontSize="normal" fontWeight="bold">
                 {order.quantityRemaining}
               </Text>
-            </Box>
+            </div>
 
-            <Box display="flex" flexDirection="column" gap="1">
+            <div className="flex flex-col gap-1">
               <Text color="text50" fontSize="xsmall" fontWeight="bold">
                 Price
               </Text>
@@ -58,9 +58,9 @@ const OrdersTableRow = ({
                 )}{' '}
                 {currency?.symbol}
               </Text>
-            </Box>
+            </div>
 
-            <Box display="flex" flexDirection="column" gap="1">
+            <div className="flex flex-col gap-1">
               <Text color="text50" fontSize="xsmall" fontWeight="bold">
                 Time left
               </Text>
@@ -68,24 +68,21 @@ const OrdersTableRow = ({
               <Text color="text100" fontSize="normal" fontWeight="bold">
                 {formatDistance(order.validUntil, new Date())}
               </Text>
-            </Box>
-          </Box>
+            </div>
+          </div>
 
-          <Box
-            display="flex"
-            alignItems="flex-end"
-            justifyContent="space-between"
-            gap="6"
+          <div
+            className="flex items-end justify-between gap-6"
           >
-            <Box display="flex" flexDirection="column" gap="1" flexGrow="1">
+            <div className="flex flex-col gap-1 flex-grow">
               <Text color="text50" fontSize="xsmall" fontWeight="bold">
                 By
               </Text>
 
               <AddressPill address={order.createdBy} />
-            </Box>
+            </div>
 
-            <Box display="flex" flexDirection="column" gap="1">
+            <div className="flex flex-col gap-1">
               <Text color="text50" fontSize="xsmall" fontWeight="bold">
                 On
               </Text>
@@ -94,19 +91,19 @@ const OrdersTableRow = ({
                 marketplace={order.marketplace}
                 originName={order.originName}
               />
-            </Box>
+            </div>
 
             {accountAddress && (
               <Table.Cell className="p-0">
                 <OrdersTableAction
-                  chainId={String(chainId)}
+                  chainId={chainId}
                   collectionAddress={collectionContractAddress as Hex}
                   tokenId={tokenId}
                   order={order}
                 />
               </Table.Cell>
             )}
-          </Box>
+          </div>
         </Table.Cell>
       </Table.Row>
 
